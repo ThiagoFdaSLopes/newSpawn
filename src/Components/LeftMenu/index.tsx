ddcc95a68e2550d9e5093faeaa27b036fc4eb0e2
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-04 15:09:31 -0300

    feat: Person

--- src/Components/LeftMenu/index.tsx
+++ src/Components/LeftMenu/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { LeftMenuComponent, ButtonSelectComponent, InfoPersonMenu } from './style'
+import { PersonagemSpawn } from '../App'
 
 type Props = {
   color: string
+  person: PersonagemSpawn
 }
 
-const CharacterMenu: React.FC<Props> = ({ color }) => {
+const CharacterMenu: React.FC<Props> = ({ color, person }) => {
   return (
     <LeftMenuComponent color={color}>
-      <InfoPersonMenu />
+      <InfoPersonMenu>
+        <p>`${person.Nome}`</p>
+      </InfoPersonMenu>
       <ButtonSelectComponent />
     </LeftMenuComponent>
   )

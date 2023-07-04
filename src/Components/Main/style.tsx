01428a7ea0337b8e696cb894122205806405dc60
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-04 10:37:50 -0300

    feat: Main Style

---
+++ src/Components/Main/style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ComponentMain = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background: radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%);
+`

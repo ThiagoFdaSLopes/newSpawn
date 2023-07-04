e787f31df5c9e78b1b71799f0e25684df2f36f94
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-04 15:09:04 -0300

    feat: Creator Style

---
+++ src/Components/CharacterCreator/style.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const CharacterCreatorComponent = styled.section`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 30vw;
+    height: 90vh;
+    background: rgba(255, 255, 255, 0.05), lightgrey 50%, ;
+    border: 2px solid white;
+    border-radius: 0.5rem;
+    margin-left: 1rem;
+`;
+
+export const H1TextComponent = styled.h1`
+    color: white;
+    font-family: 'Gotham Black', sans-serif;
+    font-weight: 500;
+    letter-spacing: 0.05rem;
+    margin-top: 2rem;
+`;

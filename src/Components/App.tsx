00d526dfba3e6977ee25f75b919bfa5382b43c06
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-04 15:08:49 -0300

    feat: new Interface and useNuiEvent

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { debugData } from '../utils/debugData';
 import Main from './Main';
 import CharacterMenu from './LeftMenu';
+import CreatorCharacter from './CharacterCreator';
+import { useNuiEvent } from '../hooks/useNuiEvent';
+
+export interface PersonagemSpawn {
+  Banco: number,
+  Barber: Number[],
+  Blood: string,
+  Clothes: {},
+  Nome: string,
+  Passport: number,
+  Sexo: string,
+  Skin: string,
+  Tattoos: [],
+}
 
 
 debugData([
@@ -12,11 +26,18 @@ debugData([
 ])
 
 const App: React.FC = () => {
+  const [personagens, setPersonagens] = useState<PersonagemSpawn[] | []>([])
+
+  useNuiEvent<PersonagemSpawn[] | []>("Spawn", (data) => {
+    setPersonagens(data)
+  })
+
   return (
     <Main>
-      <CharacterMenu color="red"/>
-      <CharacterMenu color="blue"/>
-      <CharacterMenu color="white"/>
+      {
+        personagens.length === 0 ? <CreatorCharacter /> :
+        personagens.map((e) => <CharacterMenu color="red" person={e}/>)
+      }
     </Main>
   )
 }

5f439ade398e27d647ca111a13fadcaeae0c3c21
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-07-04 15:08:55 -0300

    feat: Creator Character

---
+++ src/Components/CharacterCreator/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CharacterCreatorComponent, H1TextComponent } from './style';
+
+const CreatorCharacter = () => {
+  return (
+    <>
+      <CharacterCreatorComponent>
+        <H1TextComponent>CRIADOR DE PERSONAGEM</H1TextComponent>
+      </CharacterCreatorComponent>
+    </>
+  )
+}
+
+export default CreatorCharacter
